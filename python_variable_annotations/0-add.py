0e825fc2b68a52a486705b9c1663e131e2870274
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-02 14:26:03 -0600

    returns a float value

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """annointed addition function"""
 
-def add(a:float, b:float) -> float
+def add(a: float, b: float) -> float
 """adds a+b returns float"""
-return a+b 
+return a + b 
