c54ffe0a83bd4599a8e0399b72d53585b56d5432
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-10 19:02:07 -0600

    return

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python3
-"""
-2. Hypermedia pagination
-
-Copies in function named `index_range`
-from pagination/0-simple_helper_function.py
-
-Implement a method named `get_page` that takes two integer arguments
-`page` with default value 1 - and - `page_size` with default value 10.
-
-You have to use the CSV file `Popular_Baby_Names.csv`
-
-Use assert to verify that both arguments are integers greater than 0.
-
-Use index_range to find the correct indexes to paginate the dataset correctly
-and return the appropriate page of the dataset (i.e. the correct list of rows).
-
-If the input arguments are out of range for the dataset,
-an empty list should be returned.
-"""
+"""2-hypermedia"""
 
 import csv
 import math
