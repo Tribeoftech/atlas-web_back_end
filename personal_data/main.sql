c84571256cc7aa92ab85a9baa001bce6b526fa26
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-15 10:55:28 -0600

    fix

--- personal_data/main.sql
+++ personal_data/main.sql
@@ -1,13 +1,14 @@
--- setup mysql server
--- configure permissions
+-- Setup MySQL server
+-- Configure permissions
 CREATE DATABASE IF NOT EXISTS my_db;
-CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY 'root';
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON my_db.* TO 'root'@'localhost';
 
 USE my_db;
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+-- DROP TABLE IF EXISTS users; -- This line is commented out to avoid accidental deletion of the table
+
+CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(256),
     email VARCHAR(256),
     phone INT,
@@ -15,11 +16,12 @@ CREATE TABLE users (
     password VARCHAR(256),
     ip VARCHAR(256),
     last_login INT,
-    user_agent VARCHAR(256));
-
-
-
+    user_agent VARCHAR(256)
 );
+
+-- The extra parenthesis at the end of the CREATE TABLE statement is removed
+
+
 
 INSERT INTO users(email) VALUES ("bob@dylan.com");
 INSERT INTO users(email) VALUES ("bib@dylan.com");
