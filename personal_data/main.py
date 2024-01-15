4373b45ae69d43eda79f525130b667448ffba926
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-15 11:51:50 -0600

    personal_data

--- personal_data/main.py
+++ personal_data/main.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
+
 """
 Main file
 """
-from filtered_logger import get_db
-import logging
+
+from filtered_logger import get_db, get_logger, PII_FIELDS
+
+# Get a database connection
 db = get_db()
+
+# Create a cursor
 cursor = db.cursor()
+
+# Execute an SQL query
 cursor.execute("SELECT COUNT(*) FROM users;")
+
+# Fetch and print the result
 for row in cursor:
     print(row[0])
-get_logger = __import__('filtered_logger').get_logger
-PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
-print(get_logger.__annotations__.get('return'))
-print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+# Close cursor and database connection
 cursor.close()
 db.close()
+
+# Logging-related code
+logger = get_logger()
+print(logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
