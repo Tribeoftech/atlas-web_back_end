5f9ca31a14c1bcd425b70c23228000ddd6f028cf
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-15 11:45:56 -0600

    main

--- personal_data/main.py
+++ personal_data/main.py
@@ -2,11 +2,17 @@
 """
 Main file
 """
-
+from filtered_logger import get_db
 import logging
-
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
 get_logger = __import__('filtered_logger').get_logger
 PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
 print(get_logger.__annotations__.get('return'))
 print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+cursor.close()
+db.close()
