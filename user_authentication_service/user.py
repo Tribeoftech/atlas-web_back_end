fb853ccbf3d0eff7b86e2a5dc845a0e6282cef9e
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-22 16:54:01 -0600

    fix

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -5,8 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users
- 
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
