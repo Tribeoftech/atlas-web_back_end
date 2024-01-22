8ea1a09d476612c4ebcdb55d19273e1bd014d018
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-22 13:10:22 -0600

    update app.py

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ if AUTH_TYPE == "auth":
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
@@ -50,6 +53,8 @@ def auth_before_request() -> str:
 
     Looks through certain lists of paths to see if the user is authorized
 
+    Assigns the result of auth.current_user(request) to request.current_user
+
     Returns:
         if auth is none, returns
         if the path is in the list of paths, returns. else uses require_auth()
@@ -57,23 +62,29 @@ def auth_before_request() -> str:
     Aborts:
         if auth.authorization_header(request) is None, abort 401
         if auth.current_user(request) is None, abort 403
+        if auth.authorization_header(request) and auth.session_cookie(request)
+            return None, abort 401
     """
     if auth is None:
         return
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+       auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
