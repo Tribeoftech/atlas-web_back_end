873b3a791746b3377877d15d5d84411be92ab2c3
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-22 17:01:05 -0600

    fix argument/imports

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,24 +1,30 @@
-from sqlalchemy import 
-from sqlalchemy.ext.declarative import 
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User, Base  
-  
-def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database
 
-        Args:
-            email (str): User's email
-            hashed_password (str): User's hashed password
+from user import Base
 
-        Returns:
-            User: User object added to the database
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
         """
-        # Create a new User instance
-        new_user = User(email=email, hashed_password=hashed_password)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
-        # Add the new user to the session and commit to save in the database
-        self._session.add(new_user)
-        self._session.commit()
-
-        return new_user
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
