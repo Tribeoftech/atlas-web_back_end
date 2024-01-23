e2d8213c0adc0280157541b9cfbed9f4c0b2977d
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-23 11:49:54 -0600

    def users

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Creates Flask app
+Create Flask app
 """
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
@@ -12,7 +12,24 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     """
-    First get endpoint
+    get endpoint
     """
    return jsonify({"message": "Bienvenue"})
 
+
+@app.route("/users", methods=["POST"])
+def users():
+    """
+    Expect form data: "email" "password".
+
+    If user does not exist, register it, respond.
+
+    If user exists, catch exception,return JSON payload and 400
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({'email': email, 'message:': 'user created'})
+    except ValueError as e:
+        return jsonify({'message': 'email already registered'}), 400
