a30fcc45b38c4178ba37f2a781819c59b40b6d3c
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-23 15:46:45 -0600

    user.py fix

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+"""user.py file defines the User class"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from user import User 
 
 Base = declarative_base()
 
+
 class User(Base):
-    
+    """class defines a user"""
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
