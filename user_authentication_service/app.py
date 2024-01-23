0ca6d98c36700b7640aafce23047e79bbe86668a
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-23 11:27:00 -0600

    app.py route

---
+++ user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Creates Flask app
+"""
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+
+AUTH = Auth()
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    """
+    First get endpoint
+    """
+    return jsonify({"message": "Bienvenue"})
+
