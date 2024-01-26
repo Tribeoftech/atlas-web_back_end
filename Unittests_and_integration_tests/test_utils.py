0d5a4c976492b62551b2be879263d76028141f68
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-26 15:02:37 -0600

    test_utils

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,3 +5,18 @@ Unittests for utils.py
 import unittest
 from unittest import TestCase
 
+class TestAccessNestedMap(TestCase):
+    """
+        Test access to nested map method
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, map, path, expected):
+        """
+        Test access to nested map method
+        """
+        from utils import access_nested_map
+        self.assertEqual(access_nested_map(map, path), expected)
