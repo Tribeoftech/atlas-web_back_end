293f1de3e4690aad221faa2dc812b46f5993ec04
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-26 13:34:18 -0600

    fix

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python3
 """
-Unittest for utils.py
+Unittests for utils.py
 """
 import unittest
-import unittest import TestCase
+from unittest import TestCase
 
-class TestAccessNestedMap(TestCase):
-   """
-   Test access to nestedmap
-   """
-   @parameterized.expand([
-       ({"a": 1}, ("a",), 1),
-       ({"a": {"b": 2}}, ("a", ), {"b": 2}),
-       ({"a": {"b": 2}}, ("a", "b"), 2),
-   ])
-   def test_access_nested_map(self, map, path, expected):
-       """
-       Test access
-       """
-       from utils import access_nested_map
