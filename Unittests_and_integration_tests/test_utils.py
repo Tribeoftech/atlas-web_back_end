9a61db9a1689072f55d8430eeca4a20f698964d9
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-27 12:37:31 -0600

    pycodestyle

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-unittest for utils.py
+Unittests for utils.py
 """
 import unittest
 from unittest import TestCase
@@ -9,10 +9,10 @@ from unittest import mock
 from parameterized import parameterized
 from utils import memoize
 
-class TestAccessNestedMap(TestCase):
 
+class TestAccessNestedMap(TestCase):
     """
-        Test access to map
+        Test access to nested map method
     """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -21,32 +21,47 @@ class TestAccessNestedMap(TestCase):
     ])
     def test_access_nested_map(self, map, path, expected):
         """
-        Test access to map
+        Test access to nested map method
         """
         from utils import access_nested_map
         self.assertEqual(access_nested_map(map, path), expected)
-        
-        @parameterized.expand([
+
+    @parameterized.expand([
         ({}, ("a")),
         ({"a": 1}, ("a", "b")),
     ])
-        def test_access_nested_map_exception(self, map, path):
-         from utils import access_nested_map
-         self.assertRaises(KeyError, access_nested_map, map, path)
-         
-    class TestGetJson(TestCase):
-        @parameterized.expand([
+    def test_access_nested_map_exception(self, map, path):
+        """
+        Test access to nested map method
+        """
+        from utils import access_nested_map
+        self.assertRaises(KeyError, access_nested_map, map, path)
+
+
+class TestGetJson(TestCase):
+    """
+    This class object tests retriving a json return
+    """
+    @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
-    ])    
-        def test_get_json(self, test_url, test_payload): 
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Use unittest.mock.patch to patch requests.get
+        Makes sure it returns a <Mock> obj with a <json> method
+        that returns <test_payload> which you parameterize alongside the
+        <test_url> that will pass to <get_json> with the parameterized inputs:
+        """
         from utils import get_json
         import requests
         from unittest import mock
         with mock.patch('requests.get', return_value=mock.Mock(
                 json=lambda: test_payload)):
             self.assertEqual(get_json(test_url), test_payload)
-    class TestMemoize(unittest.TestCase):
+
+
+class TestMemoize(unittest.TestCase):
     """ This is a class that holds methods for testing the utils.memoize """
 
     def test_memoize(self):
@@ -69,5 +84,6 @@ class TestAccessNestedMap(TestCase):
             fn.assert_called_once()
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     unittest.main()
+   
