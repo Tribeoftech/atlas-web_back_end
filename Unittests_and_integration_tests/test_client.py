9cfb2178a809cd8826c8dec8ebb2acd60250fb76
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-28 12:25:38 -0600

    test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-    """
-    unittest for client.py
-    """
-    TestGithubOrgClient(unittest.TestCase)
+"""
+unittest for client.py
+"""
+TestGithubOrgClient(unittest.TestCase)
+    
+@parameterized.expand([testcase.TestCase])
+def test_get_json(self, test_url, test_payload):
+ """
+ Test get_json method
+ """
+ from client import get_json
+ self.assertEqual(get_json(test_url, test_payload), test_payload)
