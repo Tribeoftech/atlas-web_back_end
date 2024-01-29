05736016b634d0f326843c7706bd5b8a554e0e0d
Tribeoftech <gratefulgrowing333@gmail.com>
2024-01-29 09:55:53 -0600

    render template

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -2,3 +2,15 @@
 """ 
 0-app.py
 """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Return the index page.
+    """
+    return render_template('0-index.html')
