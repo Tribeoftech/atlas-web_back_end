b30c55cf4140e638637cd1acb6942ea6a398bd83
Tribeoftech <gratefulgrowing333@gmail.com>
2024-02-19 13:26:53 -0600

    calculates number of lines in a file/test file

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+/**
+ * Test suite for the calculateNumber function.
+ *
+ * Tests that a and b are rounded properly using Math.round().
+ * Checks multiple test cases with different input values.
+ */
+const calculateNumber = require("./0-calcul");
+const assert = require("assert");
+
+describe("Test suite", function () {
+  it("Tests that a and b are rounded properly using Math.round()", function () {
+    assert.equal(calculateNumber(1.1, 2.2), 3);
+    assert.equal(calculateNumber(1.4, 1, 4), 2);
+    assert.equal(calculateNumber(1.5, 1.5), 4);
+    assert.equal(calculateNumber(1.6, 1.6), 4);
+    assert.equal(calculateNumber(1.7, 1), 3);
+    assert.equal(calculateNumber(1, 1.8), 3);
+    assert.equal(calculateNumber(-5, 0.5), -4);
+    assert.equal(calculateNumber(0, -10.98), -11);
+    assert.equal(calculateNumber(1.1, 1.9), 3);
+    assert.equal(calculateNumber(1.1, 0), 1);
+  });
+});
---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,24 @@
+/**
+ * Tests calculateNumber() function by passing different operations
+ * and input values and asserting the expected output.
+ * Covers cases with decimal input values to test rounding behavior.
+ * Also tests edge cases like divide by zero errors.
+ */
+const calculateNumber = require("./1-calcul");
+const assert = require("assert");
+
+describe("Test suite", function () {
+  it("Tests that a and b are rounded properly using Math.round()", function () {
+    assert.equal(calculateNumber("SUM", 1.1, 1.2), 2);
+    assert.equal(calculateNumber("SUM", 9.9, 0), 10);
+    assert.equal(calculateNumber("SUM", 1, 1.6), 3);
+    assert.equal(calculateNumber("SUBTRACT", 1.1, 1.2), 0);
+    assert.equal(calculateNumber("SUBTRACT", 9.9, 5), 5);
+    assert.equal(calculateNumber("SUBTRACT", 1, -1.1), 2);
+    assert.equal(calculateNumber("DIVIDE", 1.1, 1.2), 1);
+    assert.equal(calculateNumber("DIVIDE", 9.9, 5), 2);
+    assert.equal(calculateNumber("DIVIDE", 1, 1.1), 1);
+    assert.equal(calculateNumber("DIVIDE", 1, 0), "Error");
+    assert.equal(calculateNumber("DIVIDE", 1, 0.1), "Error");
+  });
+});
