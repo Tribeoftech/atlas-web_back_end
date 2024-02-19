e8d5f8e51f4d911b1aeb8db1e5829b5e5afcca1f
Tribeoftech <gratefulgrowing333@gmail.com>
2024-02-19 09:34:51 -0600

    PRINTS WELCOME MESS

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+console.log('Welcome to Holberton School, what is your name?');
+/**
+ * Prints a welcome message and prompts the user for their name.
+ */
+process.stdin
+  /**
+   * Reads user input from stdin when data is available
+   * and writes a greeting with their name to stdout.
+   */
+  .on("readable", () => {
+    const name = process.stdin.read();
+    if (name !== null) {
+      process.stdout.write(`Your name is: ${name}`);
+    }
+  })
