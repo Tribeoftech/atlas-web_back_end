f5bb456bee8516aa73c015427369d8f866aaa0e4
Tribeoftech <gratefulgrowing333@gmail.com>
2024-02-19 12:47:49 -0600

    http_expressions.js

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,21 @@
+/**
+ * Starts an Express server listening on the given port. 
+ * Logs a message when the server starts.
+ */
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+/**
+ * Logs a message indicating the Express server has started listening on the given port.
+ */
+app.listen(port, () => {
+  console.log('Server is running');
+});
+
+module.exports = app;
